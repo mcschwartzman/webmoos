a01ca92889edc898c2aaebb1367b8b7a24737f87
mschwartzman2 <mschwartzman@sea-machines.com>
2025-04-30 17:26:30 -0400

    feat: adds fleet docker compose

--- autonomy/docker_entrypoint.sh
+++ autonomy/docker_entrypoint.sh
@@ -2,4 +2,14 @@
 
 cd /missions/henry_gilda_baseline
 
-./launch.sh $TIMEWARP
+# check some env vars
+if [[ $VNAME == "" ]];
+then
+    # if vname is empty, must be shoreside
+    echo "running shoreside"
+    ./launch_shoreside.sh --mport=$MPORT --pshare=$PSHARE $TIMEWARP
+else
+    # otherwise run this with the vname
+    echo "running vehicle ($VNAME)"
+    ./launch_vehicle.sh --mport=$MPORT --pshare=$PSHARE $TIMEWARP
+fi;
