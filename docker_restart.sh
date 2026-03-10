9d8773751392be05ac5c45f512b9aa2d2c48d2b2
Mathew Schwartzman <mschwartzman@sea-machines.com>
2026-03-10 18:27:37 -0400

    feat: adds amd64 dc

--- docker_restart.sh
+++ docker_restart.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-docker compose -f docker-compose.arm.yaml down &&
+docker compose -f docker-compose.amd64.yaml down &&
 # docker image rm webmoos-api &&
-# docker rm webmoos-mathew-1 webmoos-shoreside-1 webmoos-clayton-1 webmoos-josh-1 webmoos-jeremy-1
-# docker image rm webmoos-mathew webmoos-shoreside webmoos-clayton webmoos-josh webmoos-jeremy
+docker rm webmoos-mathew-1 webmoos-shoreside-1 webmoos-clayton-1 webmoos-josh-1 webmoos-jeremy-1
+docker image rm webmoos-mathew webmoos-shoreside webmoos-clayton webmoos-josh webmoos-jeremy
 # docker image rm webmoos-ui &&
-docker compose -f docker-compose.arm.yaml up
+docker compose -f docker-compose.amd64.yaml up
 
 
 
